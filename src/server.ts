94a85c85f74a3d97d05331900e8cf270f73f82b3
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2024-03-14 19:06:12 -0700

    connected my database and able to successfully create a user

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,17 @@ require("dotenv").config();
 import userRoute from "./routes/user";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI || "";
 const app = express();
+
+mongoose
+  .connect(MONGO_URI, { dbName: "test" })
+  .then((): void => console.log("[SERVER]: Database is connected"))
+  .catch((err: string): void =>
+    console.log("[ERROR]: Database is not connected", err)
+  );
 
 app.use(express.json());
 app.use(cors());
